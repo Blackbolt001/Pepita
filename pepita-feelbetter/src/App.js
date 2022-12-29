a5ddfcd15c155e8671595f2241e43b4fd62a2546
Joseph Gossett <105749663+Blackbolt001@users.noreply.github.com>
2022-12-28 23:57:42 -0500

    first push

---
+++ pepita-feelbetter/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          OOh La La 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+       by me coffee uWu :) 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
